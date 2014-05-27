7eeacb5a967374a052a9a34f284195f768801e93
Bruno Teixeira <bruno.teixeira@zalando.de>
2014-05-27 18:15:15 +0200

    Added scanning of the $PATH. All commands are added. Added new command "all" to list all available commands retrieved from $PATH.

--- src/commands.js
+++ src/commands.js
@@ -1,7 +1,67 @@
+var fs = require('fs');
+var path = require('path');
+
+var spawn = require('child_process').spawn;
 
 var Commands = function () {
 
 };
+
+var commands = {
+    cd: cd,
+    stub: stub,
+    exit: exit,
+    all: all
+};
+
+var cache = {};
+
+var paths = process.env.PATH.split(path.delimiter);
+paths.forEach(function (dir) {
+    console.log(dir);
+    var files;
+    try {
+        files = fs.readdirSync(dir);
+    } catch (ex) {
+        return;
+    }
+    files.forEach(function (file) {
+        file = path.resolve(dir, file);
+//        console.log('\t', file);
+        var stat;
+        try {
+            stat = fs.statSync(file);
+        } catch (ex) {
+        }
+        if (stat && stat.mode & 0111) { // test for *any* of the execute bits
+            addCmd(file);
+        }
+    });
+});
+
+function addCmd (file) {
+    var name = path.basename(file);
+//    console.log('adding', file);
+    if (! (name in commands)) {
+        cache[name] = file;
+        commands[name] = makeCmd(file);
+    }
+}
+
+function run (file, args) {
+    spawn(file, args, {
+        cwd: process.cwd(),
+        env: process.env,
+        stdio: 'inherit'
+    });
+}
+
+function makeCmd (file) {
+    return function () {
+//        console.log('will now run', file);
+        run (file, Array.prototype.slice.call(arguments, 0));
+    }
+}
 
 function cd (dir) {
     //console.log ('changing to', dir);
@@ -17,11 +77,9 @@ function exit () {
     process.exit();
 }
 
-var commands = {
-    cd: cd,
-    stub: stub,
-    exit: exit
-};
+function all () {
+    console.log(Object.keys(commands));
+}
 
 Commands.prototype.getCmd = function (cmd) {
     return commands[cmd];
