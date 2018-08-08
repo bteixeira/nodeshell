25da6485fd414cfeef67f2b023c0eda50a5203fd
Bruno Teixeira <bteixeira666@gmail.com>
2018-08-08 11:50:34 +0200

    Changed scripts to use source maps.

--- src/tape.ts
+++ src/tape.ts
@@ -63,7 +63,6 @@ export default class Tape<T> {
 	}
 
 	/**
-	 *
 	 * Returns the character at current position without changing the pointer.
 	 * @returns {string} the character at the current position
 	 */
