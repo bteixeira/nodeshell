a63470fc3fb89ae3f46eb279bdc85ca9d0f2f6df
Bruno Teixeira <bteixeira666@gmail.com>
2018-08-22 18:35:37 +0200

    Removed layout settings from the default user file.

--- .nsh.js
+++ .nsh.js
@@ -4,7 +4,7 @@ var utils = require(nsh.home + '/utils');
 var fs = require('fs');
 
 (function () {
-	var hostname = require('os').hostname();
+	// var hostname = require('os').hostname();
 	var gitBranchCmd = 'git symbolic-ref --short HEAD 2>/dev/null';
 	var gitDirtyCmd = 'git status --porcelain';
 	var execSync = require('child_process').execSync;
@@ -45,70 +45,70 @@ var fs = require('fs');
 	});
 })();
 
-nsh.setLayout({
-	cols: [
-		{
-			width: 'auto',
-			rows: [
-				{name: 'prompt'},
-				{name: 'completions'},
-			],
-		}, {
-			name: 'separator',
-			width: 3,
-		}, {
-			name: 'sidebar',
-			width: 40,
-		},
-	],
-});
-
-nsh.layout.separator.setRedraw(function () {
-	this.write(colors.blue(' \u2502  \u2502  \u2502  \u2502  \u2502'));
-});
-nsh.layout.sidebar.setRedraw(function () {
-	var me = this;
-	fs.readdir('.', function (err, files) {
-		if (err) {
-			me.write(colors.red(err.toString()));
-		} else {
-			if (files.length <= 5) {
-				me.write(files.join('\n') + '\n');
-			} else {
-				me.write(files.slice(0, 4).join('\n') + '\n');
-				me.write(colors.grey('  (' + (files.length - 4) + ' more...)'))
-			}
-		}
-	});
-});
-nsh.layout.completions.setRedraw(function () {
-	// this.write('Completions go here...');
-});
-
-/**/
-// TODO THIS IS KINDA HOW IT SHOULD BE
-nsh.on('before-execute before-exit', function () {
-	nsh.layout.separator.clear();
-	nsh.layout.sidebar.clear();
-	nsh.layout.completions.clear();
-});
-nsh.on('before-prompt', function () {
-	nsh.layout.separator.write(colors.blue(' \u2502  \u2502  \u2502  \u2502  \u2502'));
-});
-nsh.lineReader.on('update', function () {
-	// TODO THIS SHOULD TRY TO COMPLETE THE LAST TOKEN WITH A FILENAME, MAYBE THIS SHOULDN'T BE CALLED ON THE lineReader BUT ON THE PARSER OR SOMETHING
-	var me = this;
-	fs.readdir('.', function (err, files) {
-		if (err) {
-			me.write(colors.red(err.toString()));
-		} else {
-			if (files.length <= 5) {
-				me.write(files.join('\n') + '\n');
-			} else {
-				me.write(files.slice(0, 4).join('\n') + '\n');
-				me.write(colors.grey('  (' + (files.length - 4) + ' more...)'))
-			}
-		}
-	});
-});
+// nsh.setLayout({
+// 	cols: [
+// 		{
+// 			width: 'auto',
+// 			rows: [
+// 				{name: 'prompt'},
+// 				{name: 'completions'},
+// 			],
+// 		}, {
+// 			name: 'separator',
+// 			width: 3,
+// 		}, {
+// 			name: 'sidebar',
+// 			width: 40,
+// 		},
+// 	],
+// });
+//
+// nsh.layout.separator.setRedraw(function () {
+// 	this.write(colors.blue(' \u2502  \u2502  \u2502  \u2502  \u2502'));
+// });
+// nsh.layout.sidebar.setRedraw(function () {
+// 	var me = this;
+// 	fs.readdir('.', function (err, files) {
+// 		if (err) {
+// 			me.write(colors.red(err.toString()));
+// 		} else {
+// 			if (files.length <= 5) {
+// 				me.write(files.join('\n') + '\n');
+// 			} else {
+// 				me.write(files.slice(0, 4).join('\n') + '\n');
+// 				me.write(colors.grey('  (' + (files.length - 4) + ' more...)'))
+// 			}
+// 		}
+// 	});
+// });
+// nsh.layout.completions.setRedraw(function () {
+// 	// this.write('Completions go here...');
+// });
+//
+// /**/
+// // TODO THIS IS KINDA HOW IT SHOULD BE
+// nsh.on('before-execute before-exit', function () {
+// 	nsh.layout.separator.clear();
+// 	nsh.layout.sidebar.clear();
+// 	nsh.layout.completions.clear();
+// });
+// nsh.on('before-prompt', function () {
+// 	nsh.layout.separator.write(colors.blue(' \u2502  \u2502  \u2502  \u2502  \u2502'));
+// });
+// nsh.lineReader.on('update', function () {
+// 	// TODO THIS SHOULD TRY TO COMPLETE THE LAST TOKEN WITH A FILENAME, MAYBE THIS SHOULDN'T BE CALLED ON THE lineReader BUT ON THE PARSER OR SOMETHING
+// 	var me = this;
+// 	fs.readdir('.', function (err, files) {
+// 		if (err) {
+// 			me.write(colors.red(err.toString()));
+// 		} else {
+// 			if (files.length <= 5) {
+// 				me.write(files.join('\n') + '\n');
+// 			} else {
+// 				me.write(files.slice(0, 4).join('\n') + '\n');
+// 				me.write(colors.grey('  (' + (files.length - 4) + ' more...)'))
+// 			}
+// 		}
+// 	});
+// });
 // nsh.completions.setPanel(nsh.layout.completions);
